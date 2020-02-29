31ecb8a4e40be959dda6f7f7a82c8a13fb6599bd
aeinrw <aeinrw@gmail.com>
2020-03-01 03:15:32 +0800

    重写了T的递归,但好像不对...

--- effictive/test.py
+++ effictive/test.py
@@ -29,10 +29,16 @@ class LIME:
     def __toeplitzMatrix(self):
         self.d_v = self.__firstOrderDerivative(self.row)
         self.d_h = self.__firstOrderDerivative(self.col, -1)
-        self.D_v = np.kron(np.identity(self.col), self.d_v)
-        self.D_h = np.kron(self.d_h.T, np.identity(self.row))
-        self.D = np.vstack([self.D_v, self.D_h])
-        return self.D
+        # self.D_v = np.kron(np.identity(self.col), self.d_v)
+        # self.D_h = np.kron(self.d_h.T, np.identity(self.row))
+        # self.D = np.vstack([self.D_v, self.D_h])
+        self.vecDD = np.zeros(self.row * self.col)
+        self.vecDD[0] = 4
+        self.vecDD[1] = -1
+        self.vecDD[self.row] = -1
+        self.vecDD[-1] = -1
+        self.vecDD[-self.row] = -1
+        # return self.D
 
     def __firstOrderDerivative(self, n, k=1):
         return (np.eye(n)) * (-1) + np.eye(n, k=k)
@@ -47,6 +53,7 @@ class LIME:
         threshold = np.linalg.norm(self.T_hat, ord='fro') * 0.00001
 
         while (np.linalg.norm((self.__derivativeOfT(T) - G), ord='fro') <= threshold) and t < 20:
+            input("--->")
             T = self.__T_subproblem(G, Z, u)
             G = self.__G_subproblem(T, Z, u, self.W)
             Z = self.__Z_subproblem(T, G, Z, u)
@@ -58,22 +65,13 @@ class LIME:
 
     def __T_subproblem(self, G, Z, u):
         # G(t) Z(t) u(t)
-
-        tem = u * (self.D.T) @ (self.__ravel(G - Z / u))
-        left = (2 * self.T_hat).T.ravel() + tem
-        left = left.reshape((1, -1))
-        numerator = sp.fft.fft2(left)
-        print(numerator.shape)
-        # right = u * (self.D.T) @ self.D
-        # right[np.diag_indices_from(right)] += 2
-        # return self.__stack(np.linalg.solve(right, left))
-        h = sp.fft.fft2(self.D_h[0, :].reshape((1, -1)))
-        v = sp.fft.fft2(self.D_v[0, :].reshape((1, -1)))
-        denominator = 2 + u * (h.conj()*h + v.conj()*v)
-        print(denominator.shape)
-        input("dd")
-        T = sp.fft.ifft2(numerator / denominator)
-        return self.__stack(np.real(T))
+        denominator = sp.fft.fft(self.vecDD * u) + 2
+        temp1 = G - Z / u
+        temp2 = self.d_v @ (temp1[:self.row, :]) + \
+            (temp1[self.row:, :]) @ self.d_h
+        numerator = sp.fft.fft((self.T_hat + u * temp2).T.ravel())
+        T = sp.fft.ifft(denominator / numerator)
+        return np.real(self.__stack(T))
 
     def __ravel(self, matrix):
         # 按列展开
@@ -90,10 +88,10 @@ class LIME:
         epsilon = self.alpha * W / u
         return np.sign(np.maximum(np.abs(dT + Z / u) - epsilon, 0))
 
-    def __derivativeOfT(self, T):
-        Tv = self.d_v @ T
-        Th = T @ self.d_h
-        return np.vstack([Tv, Th])
+    def __derivativeOfT(self, matrix):
+        v = self.d_v @ matrix
+        h = matrix @ self.d_h
+        return np.vstack([v, h])
 
     def __Z_subproblem(self, T, G, Z, u):
         # Z(t) u(t) G(t+1) T(t+1)
@@ -112,6 +110,7 @@ class LIME:
     def enhance(self, nameta=0.9):
         tem = self.optimizeIllumMap()
         io.imsave('./T.jpg', img_as_ubyte(tem))
+        print(np.max(tem))
         self.R = np.zeros(self.L.shape)
         for i in range(3):
             self.R[:, :, i] = 1 - \
@@ -122,5 +121,5 @@ class LIME:
 
 
 if __name__ == "__main__":
-    lime = LIME("C:/Source/LIME/effictive/L.bmp")
+    lime = LIME("C:/Source/LIME/effictive/3.bmp")
     io.imsave('./R.jpg', lime.enhance())
