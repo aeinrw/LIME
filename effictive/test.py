da0358dc3e0147d698007b14d37865e1e2ff1686
aeinrw <aeinrw@gmail.com>
2020-02-29 21:32:19 +0800

    用FFT加速,但还是很慢

--- effictive/test.py
+++ effictive/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 from skimage import io, exposure, filters, img_as_float, img_as_ubyte
 import matplotlib.pyplot as plt
 
@@ -28,9 +29,9 @@ class LIME:
     def __toeplitzMatrix(self):
         self.d_v = self.__firstOrderDerivative(self.row)
         self.d_h = self.__firstOrderDerivative(self.col, -1)
-        D_v = np.kron(np.identity(self.col), self.d_v)
-        D_h = np.kron(self.d_h.T, np.identity(self.row))
-        self.D = np.vstack([D_v, D_h])
+        self.D_v = np.kron(np.identity(self.col), self.d_v)
+        self.D_h = np.kron(self.d_h.T, np.identity(self.row))
+        self.D = np.vstack([self.D_v, self.D_h])
         return self.D
 
     def __firstOrderDerivative(self, n, k=1):
@@ -57,11 +58,22 @@ class LIME:
 
     def __T_subproblem(self, G, Z, u):
         # G(t) Z(t) u(t)
+
         tem = u * (self.D.T) @ (self.__ravel(G - Z / u))
         left = (2 * self.T_hat).T.ravel() + tem
-        right = u * (self.D.T) @ self.D
-        right[np.diag_indices_from(right)] += 2
-        return self.__stack(np.linalg.solve(right, left))
+        left = left.reshape((1, -1))
+        numerator = sp.fft.fft2(left)
+        print(numerator.shape)
+        # right = u * (self.D.T) @ self.D
+        # right[np.diag_indices_from(right)] += 2
+        # return self.__stack(np.linalg.solve(right, left))
+        h = sp.fft.fft2(self.D_h[0, :].reshape((1, -1)))
+        v = sp.fft.fft2(self.D_v[0, :].reshape((1, -1)))
+        denominator = 2 + u * (h.conj()*h + v.conj()*v)
+        print(denominator.shape)
+        input("dd")
+        T = sp.fft.ifft2(numerator / denominator)
+        return self.__stack(np.real(T))
 
     def __ravel(self, matrix):
         # 按列展开
@@ -99,7 +111,7 @@ class LIME:
 
     def enhance(self, nameta=0.9):
         tem = self.optimizeIllumMap()
-        io.imsave('./reuslt5.jpg', tem)
+        io.imsave('./T.jpg', img_as_ubyte(tem))
         self.R = np.zeros(self.L.shape)
         for i in range(3):
             self.R[:, :, i] = 1 - \
@@ -110,5 +122,5 @@ class LIME:
 
 
 if __name__ == "__main__":
-    lime = LIME("C:/Source/LIME/data/small.bmp")
-    io.imsave('./result4.jpg', lime.enhance())
+    lime = LIME("C:/Source/LIME/effictive/L.bmp")
+    io.imsave('./R.jpg', lime.enhance())
